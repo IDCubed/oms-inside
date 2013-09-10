5ba6edd2f99862cbaad9d56a86846e676d3fd1f3
Adam Laughlin <adam.laughlin@gmail.com>
2013-09-10 12:35:18 -0400

    cleaned up and documented

--- docs/js/main.js
+++ docs/js/main.js
@@ -25,67 +25,66 @@
   });
 
 
-
-  var markDownContainer={
-    $el:$('#markdown-container'), // the container element for rendering the converted markdown;
-    // renders the markdown into the markdowncontainer
+  // the container element for rendering the converted markdown;
+  var markDownContainer = {
+    $el:$('#markdown-container'), 
+    // converts markdown to HTML
     convertMarkdownToHTML:function(data, textStatus, jqXHR){
       return marked(data, function (err, content) {
         if (err) return this.failedConversion(jqXHR, textStatus, err);
-        return content;
+        return content === '' ?
+          'No content to render.':
+          content;
       });
     },
+    // renders the markdown into the markdowncontainer
     render:function(data, textStatus, jqXHR){
-      this.$el.html( this.convertMarkdownToHTML(data) );
+      markDownContainer.$el.html( markDownContainer.convertMarkdownToHTML(data) );
     },
+    // handles conversion errors
     failedConversion:function(jqXHR, textStatus, err){
       console.log('Markdown render error: (msg,jqXHR)', jqXHR, textStatus, err);
       return '<p class="error">Error rendering markdown. Please check your console.</p>';
     }
   };
 
+  // the navigation element to select different documentation parts
   var nav = {
     $el : $('#docs-nav'),
-    $navItems : null,
     loadFail: function(jqXHR, textStatus, errorThrown){
-      if(!console) return;
-      if(!console.error) console.error = console.log;
-      markDownContainer.render('<p class="error">Error loading markdown. Please check your console.</p>')
+      markDownContainer.render('<p class="error">Error loading markdown. Please check your console.</p>');
       console.error('Load Error: (jqXHR, textStatus, errorThrown)', jqXHR, textStatus, errorThrown);
     },
     load:function(urlStr){
+      // load the file at an url string and render it
       $.ajax({
         url:urlStr,
         dataType:'text'
       })
-      .done(markDownContainer.renderMarkdown)
-      .fail(self.loadFail)
+      .done(function(data, textStatus, jqXHR){
+        nav.highlightCurrentNavPosition(this);
+        markDownContainer.render(data, textStatus, jqXHR);
+      })
+      .fail(this.loadFail);
     },
     init:function(){
-      var self = this;
-      this.$navItems = this.$el.find('li');
-      this.$el.on('click','a',function(event){
+      // initialize the navigation
+      nav.$el.on('click','a',function(event){
         event.preventDefault();
-
-        // load the markdown asynchronously when the nav is clicked.
-        self.highlightCurrentNavPosition(this);
-        // load the markdown
+        // load the resource based on the link's href
         self.load(this.href);
-      })
+      });
     },
     highlightCurrentNavPosition:function(linkElem){
-      this.$navItems.removeClass('active');
+      // highlight the currently active nav element
+      this.$el.find('li').removeClass('active');
       $(linkElem).closest('li').addClass('active');
     }
   };
 
+  // Initialize
   nav.init();
-  nav.load('markdowntest.md');
-  
-
-
-
-  // Using async version of marked
-
+  // load a default page
+  nav.load('overview.md');
 
 })(jQuery);
